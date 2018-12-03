5d85961415ed369213c327d45c76854b0c4f7034
mbogdan2704 <costin_bogdan.maria@stud.acs.upb.ro>
2018-12-03 21:18:47 +0200

    Add files via upload

--- myScript.js
+++ myScript.js
@@ -8,11 +8,12 @@ function tabulateAnswers() {
   // get a list of the radio inputs on the page
   var choices = document.getElementsByTagName('input');
   // loop through all the radio inputs
+  var nr = 0;
   for (i=0; i<choices.length; i++) {
     // if the radio is checked..
     if (choices[i].checked) {
       // add 1 to that choice's score
-
+      nr ++ ;
       if (choices[i].value == 'c1') {
         ok = 1
       }
@@ -20,6 +21,9 @@ function tabulateAnswers() {
       // If you add more choices and outcomes, you must add another if statement below.
     }
   }
+
+  
+
   
   // Find out which choice got the highest score.
   // If you add more choices and outcomes, you must add the variable here.
@@ -27,11 +31,19 @@ function tabulateAnswers() {
   
   // Display answer corresponding to that choice
   var answerbox = document.getElementById('answer');
-  if (ok == 0) { // If user chooses the first choice the most, this outcome will be displayed.
-    answerbox.innerHTML = "Va multumit! Va rugam sa completati formularul pentru donare"
+  if(nr != 11) 
+  {
+    answerbox.innerHTML = "Va rugam raspundeti la toate intrebarile"
+    document.getElementById("link").style.display="none";
+  }
+  else if (ok == 0) { // If user chooses the first choice the most, this outcome will be displayed.
+    answerbox.innerHTML = "Va multumit! Va rugam sa completati formularul pentru donare" 
+    document.getElementById("link").style.display="block";
   }
   else {
-    answerbox.innerHTML = "Ne pare rau, din pacate momentan nu puteti dona. Va rugam, incercati peste 6 luni."
+    document.getElementById("link").style.display="none";
+
+    answerbox.innerHTML = "Ne pare rau, din pacate momentan nu puteti dona. Va rugam, incercati peste 6 luni.";
   }
  
   // If you add more choices, you must add another response below.
@@ -56,4 +68,7 @@ function fill_as_email() {
   //as_email befüllen:
   document.querySelector("#as_email").value = as_email;
 }
+
+// SIGNIN
+
 
